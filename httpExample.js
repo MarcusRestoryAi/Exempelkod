f96be5c03c930d352f2e1cd63901222460d2424a
Marcus <marcus.henriksson@restory.ai>
2023-04-20 10:33:53 +0200

    Init npm and nodemon

--- httpExample.js
+++ httpExample.js
@@ -5,11 +5,11 @@ const url = require("url");
 //Starta up en local server
 http.createServer(function (request, response) {
     //Inkludera HTTP Code och innehåll
-    response.writeHead(200, {'Content-Type': 'text/html', 'encode': 'utf8'});
+    response.writeHead(200, {'Content-Type': 'text/html', 'encode': 'utf-8'});
     //Skriv ett meddelande tillbaka till user
     response.write("Hello World ");
-    response.write(request.url);
+    //response.write(request.url);
     let qData = url.parse(request.url, true).query;
-    let responeString = `Idag är det den ${qData.day} ${qData.month}`;
+    let responeString = `Today it's the ${qData.day} of ${qData.month}. Fin dag idag`;
     response.end(responeString); //Detta är slutet av responsen
 }).listen(8080);
